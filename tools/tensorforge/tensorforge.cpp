4ab9191f94fb7eb977df1bff0dddcf5dc3962735
Braden Helmer <bradenhelmeraus@gmail.com>
2025-08-25 20:48:55 -0400

    first executable linking properly

--- tools/tensorforge/tensorforge.cpp
+++ tools/tensorforge/tensorforge.cpp
@@ -1,7 +1,24 @@
 // tensorforge.cpp
-#include <iostream>
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+
+using namespace llvm;
+
+// Command Line Options
+
+static cl::opt<std::string> inputFilename(cl::Positional,
+                                          cl::desc("<input tensorforge file>"),
+                                          cl::Required);
+
+static cl::opt<std::string> outputFilename("o", cl::desc("Output filename"),
+                                           cl::value_desc("filename"),
+                                           cl::init("-"));
 
 int main(int argc, char *argv[]) {
-  std::cout << "tensorforge compiler v0.0.1\n";
+
+  InitLLVM X(argc, argv);
+
+  cl::ParseCommandLineOptions(argc, argv, "TensorForge Compiler\n");
+
   return 0;
 }
